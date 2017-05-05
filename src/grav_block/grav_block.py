52b95b86d8b6693db443c93839fe0da1f43b0ebf
Bell Codo <bellcodo@gmail.com>
2017-05-04 22:20:01 -0700

    Update grav_block.py

--- src/grav_block/grav_block.py
+++ src/grav_block/grav_block.py
@@ -9,7 +9,8 @@ class Constants:
 
     GRID_WIDTH = WINDOW_WIDTH / GLOBAL_DEFAULT_SQUARE_SIZE
     GRID_HEIGHT = GRID_WIDTH
-
+    SQRS = []
+#
 class Rect:
     
     def rect_coords (length, height, startpos = (0, 0)) :
@@ -44,13 +45,18 @@ class Rect:
         def set_color(self, color):
             self.shape_attributes["fill_color"] = color
             
-        def update_location(self, new_pos, edges=[Constants.GRID_HEIGHT, 15]):
+        def update_location(self, new_pos, edges=[Constants.GRID_HEIGHT]):
             check_bottom_edge = new_pos[1] + 1
-
+            
+            same_col = filter(lambda same_col: same_col.top_left_point[0] == new_pos[0], Constants.SQRS)
+            if len(same_col) > 1:
+                edges = [Constants.GRID_HEIGHT, same_col[1].top_left_point[1]]
+            
             if filter(lambda x: check_bottom_edge > x, edges):
                 self.updating = False
                 
             self.top_left_point = new_pos
+            edges = []
             
             
         def draw_me(self, canvas):
@@ -66,33 +72,23 @@ class Rect:
             if self.updating:
                 (x, y) = self.top_left_point
                 self.update_location((x, y+.05))
-class BlockMap:
-    
-    def __init__(self):
-        self.block_map = {}
-        
-    def update(self, new_block):
-        self.block_map[str(new_block.top_left_point[0]) + "_" + str(new_block.top_left_point[1])] = new_block
-     
-    def add_all(self, block_list):
-        map(self.update, block_list)
+#
 
-    def block_list(self):
-        return self.block_map.values()
-# END OF API
 
-bm = BlockMap()
+sqrs = Constants.SQRS
 
 sqr_default = Rect.Square((1,1))
 sqr_blue = Rect.Square((2,2), color="Blue")
 
-
-bm.add_all([sqr_default, sqr_blue])
-bm.add_all([ Rect.Square((i,i), updating=True) for i in range(Constants.GRID_WIDTH)])
+sqrs.extend([sqr_default, sqr_blue])
+sqrs.extend([ Rect.Square((i,i), updating=True) for i in range(Constants.GRID_WIDTH)])
+sqrs.extend([ 
+        Rect.Square((4,10)), Rect.Square((5,10)), Rect.Square((6,9))
+])
 
                                
 def draw(canvas):
-    for sqr in bm.block_list():
+    for sqr in sqrs:
         sqr.draw_me(canvas)
 
 class Graphics:   
